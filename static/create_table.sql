e4501add94a75614903507b70529830dd9e316da
Minxing Liu <Minxing Liu>
2016-05-27 16:19:57 +0800

    new sql

--- static/create_table.sql
+++ static/create_table.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2016/5/27 13:31:19                           */
+/* Created on:     2016/5/27 16:18:59                           */
 /*==============================================================*/
 
 
@@ -10,9 +10,9 @@ drop table if exists House;
 
 drop table if exists Realty;
 
-drop table if exists Save;
+drop table if exists Renter;
 
-drop table if exists User;
+drop table if exists Save;
 
 /*==============================================================*/
 /* Table: Community                                             */
@@ -38,7 +38,7 @@ create table House
    houseid              varchar(64) not null,
    com_id               varchar(64) not null,
    house_size           int,
-   avail_date           date not null,
+   availability         bool not null,
    rent                 int not null,
    bedroom              int not null,
    bathroom             int not null,
@@ -53,9 +53,21 @@ create table Realty
 (
    realty_email         varchar(64) not null,
    realty_name          varchar(64) not null,
-   realty_setupdate     date,
+   realty_setup_date    date,
    website              varchar(256),
+   realty_register_date char(10),
    primary key (realty_email)
+);
+
+/*==============================================================*/
+/* Table: Renter                                                */
+/*==============================================================*/
+create table Renter
+(
+   renter_email         varchar(64) not null,
+   renter_pswd          varchar(64) not null,
+   renter_register_date date not null,
+   primary key (renter_email)
 );
 
 /*==============================================================*/
@@ -63,21 +75,9 @@ create table Realty
 /*==============================================================*/
 create table Save
 (
-   user_email           varchar(64) not null,
+   renter_email         varchar(64) not null,
    houseid              varchar(64) not null,
-   primary key (user_email, houseid)
-);
-
-/*==============================================================*/
-/* Table: User                                                  */
-/*==============================================================*/
-create table User
-(
-   user_email           varchar(64) not null,
-   user_pswd            varchar(64) not null,
-   user_enter_date      date not null,
-   user_type            bool not null,
-   primary key (user_email)
+   primary key (renter_email, houseid)
 );
 
 alter table Community add constraint FK_Own foreign key (realty_email)
@@ -86,8 +86,8 @@ alter table Community add constraint FK_Own foreign key (realty_email)
 alter table House add constraint FK_Belong foreign key (com_id)
       references Community (com_id) on delete restrict on update restrict;
 
-alter table Save add constraint FK_Save foreign key (user_email)
-      references User (user_email) on delete restrict on update restrict;
+alter table Save add constraint FK_Save foreign key (renter_email)
+      references Renter (renter_email) on delete restrict on update restrict;
 
 alter table Save add constraint FK_Save2 foreign key (houseid)
       references House (houseid) on delete restrict on update restrict;
